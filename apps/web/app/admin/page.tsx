13c99d1760589fcdc96c9bb06b3a070dbe746898
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-11-08 01:17:16 +0530

    new Admin page with Admin Panel component

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { AdminPanel } from "@repo/ui/admin"
+
+export default function Admin(){
+    return(
+        <div>
+            <h1>
+                Welcome to 
+            </h1>
+            <AdminPanel/>   
+        </div>
+    )
+}
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,8 @@
+export const AdminPanel = ()=>{
+    return(
+        <div>
+            <h1>this is the admin pannel</h1>
+            <button>click here</button>
+        </div>
+    )
+}
