a82a0bb846b444d0486cda9c4aa22412dbd27386
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-11-11 19:04:36 +0530

    added new lines

--- packages/ui/src/admin.tsx
+++ packages/ui/src/admin.tsx
@@ -2,6 +2,7 @@ export const AdminPanel = ()=>{
     return(
         <div>
             <h1>this is the admin pannel</h1>
+            <h1>hi there </h1>
             <button>click here</button>
         </div>
     )
